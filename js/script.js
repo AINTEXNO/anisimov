968b0237470f77bb85caae2a1e9c422e73d95caa
Ivan <vanechka.anisimov.02@gmail.com>
2021-03-29 14:12:28 +0300

    all project

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ const buttonsBlock = document.querySelector('.section-buttons');
 const line = document.querySelector('.our-project__center-block');
 const projectElements = document.querySelectorAll('.our-project__element');
 
+const buttons = document.querySelector('#second-buttons');
+const items = document.querySelectorAll('.middle-content');
+const images = [
+    'img/rent-img.png',
+    'img/item-1.png',
+    'img/item-2.png'
+];
+
+const oneImage = document.querySelector('.right-content__middle-img');
+
 let currentStep = 0;
 let currentLinePosition = 0;
+let currentSlide = 0;
 
 buttonsBlock.addEventListener('click', (event) => {
     let target = event.target.id;
@@ -21,4 +32,24 @@ buttonsBlock.addEventListener('click', (event) => {
         currentLinePosition--;
     }
     line.style.left = currentStep + 'px';
+});
+
+buttons.addEventListener('click', (event) => {
+    let target = event.target.id;
+
+    if(target == 'prev-button' && currentSlide > 0) {
+        currentSlide--;
+    }
+
+    if(target == 'next-button' && currentSlide < images.length - 1) {
+        currentSlide++;
+    }
+    oneImage.src = images[currentSlide];
+    items[currentSlide].classList.add('active');
+
+    items.forEach((currentElement, currentElementIndex) => {
+        if(currentElementIndex != currentSlide && currentElement.classList.contains('active')) {
+            currentElement.classList.remove('active');
+        }
+    });
 });
